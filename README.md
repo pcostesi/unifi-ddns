0e98a37e9dc720d34525b53ef33e069b6a1e3486
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2023-08-22 16:41:02 -0300

    feat: update worker to handle lan ips
    Signed-off-by: Pablo Alejandro Costesich <pablo.costesich@gmail.com>

--- README.md
+++ README.md
@@ -11,19 +11,19 @@ You must have a Cloudflare account and your domain must be configured to point t
 
 1. Clone or download this project
 2. Ensure you have the [Wrangler CLI](https://developers.cloudflare.com/workers/wrangler/install-and-update/) installed on your system
-3. Log in with wrangler, and run `wrangler deploy`.
+3. Log in with wrangler, and run `wrangler publish`.
 4. Once you have created the worker, take note of it's \*.workers.dev route. More on routes for Cloudflare Workers [here](https://developers.cloudflare.com/workers/platform/routes#routes-with-workersdev).
 5. Create an API token so the Worker can update your DNS records. Go to https://dash.cloudflare.com/profile/api-tokens and select "Create token". On the next page, scroll down and click the "Get Started" button next to the "Create Custom Token" label. Select **Zone:DNS:Edit** for the "Permissions" drop-down, and include your target zone under the "Zone Resources" drop-down. Copy your API Key - you will need it later when configuring your UniFi OS Controller.
 
 ### Configuring UniFi OS
 1. Log on to your [UniFi OS Controller](https://unifi.ui.com/)
-2. Navigate to Settings > Internet > WAN and scroll down to **Dynamic DNS**. 
+2. Navigate to Settings > Internet > WAN and scroll down to **Dynamic DNS**.
 3. Click **Create New Dynamic DNS** and enter the following information:
 - `Service`: you must choose `dyndns`
 - `Hostname`: the full subdomain and hostname of the record you want to update (e.g. `subdomain.mydomain.com`, `mydomain.com` for root domain)
 - `Username`: the domain name containing the record (e.g. `mydomain.com`)
 - `Password`: the Cloudflare API Token you created earlier
-- `Server`: the Cloudflare Worker route `<worker-name>.<worker-subdomain>.workers.dev/update?ip=%i&hostname=%h`. 
+- `Server`: the Cloudflare Worker route `<worker-name>.<worker-subdomain>.workers.dev/update?ip=%i&hostname=%h`.
 
 #### Important Notes!
 - If you are attempting to update a subdomain (`sub.example.com`), you must manually create an A record for it **first** in your Cloudflare dashboard.
--- src/index.js
+++ src/index.js
@@ -1,173 +1,195 @@
-import { Buffer } from 'node:buffer';
+import { Buffer } from "node:buffer";
 
 class BadRequestException extends Error {
-	constructor(reason) {
-		super(reason);
-		this.status = 400;
-		this.statusText = "Bad Request";
-	}
+  constructor(reason) {
+    super(reason);
+    this.status = 400;
+    this.statusText = "Bad Request";
+  }
 }
 
 class CloudflareApiException extends Error {
-	constructor(reason) {
-		super(reason);
-		this.status = 500;
-		this.statusText = "Internal Server Error";
-	}
+  constructor(reason) {
+    super(reason);
+    this.status = 500;
+    this.statusText = "Internal Server Error";
+  }
+}
+
+function getSaneIp(ip, originalIp) {
+  // check for ipv6
+  if (ip.includes(":")) {
+    return ip;
+  }
+  // check for local ranges. If the IP is local, use the original IP
+  if (
+    ip.startsWith("10.") ||
+    ip.startsWith("192.168.") ||
+    ip.startsWith("172.16.")
+  ) {
+    return originalIp;
+  }
+  return ip;
 }
 
 class Cloudflare {
-	constructor(options) {
-		this.cloudflare_url = "https://api.cloudflare.com/client/v4";
-		this.token = options.token;
-	}
+  constructor(options) {
+    this.cloudflare_url = "https://api.cloudflare.com/client/v4";
+    this.token = options.token;
+  }
 
-	async findZone(name) {
-		const response = await this._fetchWithToken(`zones?name=${name}`);
-		const body = await response.json();
-		if (!body.success || body.result.length === 0) {
-			throw new CloudflareApiException(`Failed to find zone '${name}'`);
-		}
-		return body.result[0];
-	}
+  async findZone(name) {
+    const response = await this._fetchWithToken(`zones?name=${name}`);
+    const body = await response.json();
+    if (!body.success || body.result.length === 0) {
+      throw new CloudflareApiException(`Failed to find zone '${name}'`);
+    }
+    return body.result[0];
+  }
 
-	async findRecord(zone, name) {
-		const response = await this._fetchWithToken(`zones/${zone.id}/dns_records?name=${name}`);
-		const body = await response.json();
-		if (!body.success || body.result.length === 0) {
-			throw new CloudflareApiException(`Failed to find dns record '${name}'`);
-		}
-		return body.result[0];
-	}
+  async findRecord(zone, name) {
+    const response = await this._fetchWithToken(
+      `zones/${zone.id}/dns_records?name=${name}`
+    );
+    const body = await response.json();
+    if (!body.success || body.result.length === 0) {
+      throw new CloudflareApiException(`Failed to find dns record '${name}'`);
+    }
+    return body.result[0];
+  }
 
-	async updateRecord(record, value) {
-		record.content = value;
-		const response = await this._fetchWithToken(
-			`zones/${record.zone_id}/dns_records/${record.id}`,
-			{
-				method: "PUT",
-				body: JSON.stringify(record),
-			}
-		);
-		const body = await response.json();
-		if (!body.success) {
-			throw new CloudflareApiException("Failed to update dns record");
-		}
-		return body.result[0];
-	}
+  async updateRecord(record, value) {
+    record.content = value;
+    const response = await this._fetchWithToken(
+      `zones/${record.zone_id}/dns_records/${record.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(record),
+      }
+    );
+    const body = await response.json();
+    if (!body.success) {
+      throw new CloudflareApiException("Failed to update dns record");
+    }
+    return body.result[0];
+  }
 
-	async _fetchWithToken(endpoint, options = {}) {
-		const url = `${this.cloudflare_url}/${endpoint}`;
-		options.headers = {
-			...options.headers,
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${this.token}`,
-		};
-		return fetch(url, options);
-	}
+  async _fetchWithToken(endpoint, options = {}) {
+    const url = `${this.cloudflare_url}/${endpoint}`;
+    options.headers = {
+      ...options.headers,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${this.token}`,
+    };
+    return fetch(url, options);
+  }
 }
 
 function requireHttps(request) {
-	const { protocol } = new URL(request.url);
-	const forwardedProtocol = request.headers.get("x-forwarded-proto");
+  const { protocol } = new URL(request.url);
+  const forwardedProtocol = request.headers.get("x-forwarded-proto");
 
-	if (protocol !== "https:" || forwardedProtocol !== "https") {
-		throw new BadRequestException("Please use a HTTPS connection.");
-	}
+  if (protocol !== "https:" || forwardedProtocol !== "https") {
+    throw new BadRequestException("Please use a HTTPS connection.");
+  }
 }
 
 function parseBasicAuth(request) {
-	const Authorization = request.headers.get("Authorization");
-	const [scheme, data] = Authorization.split(" ");
-	const decoded = Buffer.from(data, 'base64').toString('ascii');
-	const index = decoded.indexOf(":");
+  const Authorization = request.headers.get("Authorization");
+  const [scheme, data] = Authorization.split(" ");
+  const decoded = Buffer.from(data, "base64").toString("ascii");
+  const index = decoded.indexOf(":");
 
-	if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
-		throw new BadRequestException("Invalid authorization value.");
-	}
+  if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
+    throw new BadRequestException("Invalid authorization value.");
+  }
 
-	return {
-		username: decoded.substring(0, index),
-		password: decoded.substring(index + 1),
-	};
+  return {
+    username: decoded.substring(0, index),
+    password: decoded.substring(index + 1),
+  };
 }
 
 async function handleRequest(request) {
-	requireHttps(request);
-	const { pathname } = new URL(request.url);
+  requireHttps(request);
+  const { pathname } = new URL(request.url);
 
-	if (pathname === "/favicon.ico" || pathname === "/robots.txt") {
-		return new Response(null, { status: 204 });
-	}
+  if (pathname === "/favicon.ico" || pathname === "/robots.txt") {
+    return new Response(null, { status: 204 });
+  }
 
-	if (pathname !== "/nic/update" && pathname !== "/update") {
-		return new Response("Not Found.", { status: 404 });
-	}
+  if (pathname !== "/nic/update" && pathname !== "/update") {
+    return new Response("Not Found.", { status: 404 });
+  }
 
-	if (!request.headers.has("Authorization")) {
-		throw new BadRequestException("Please provide valid credentials.");
-	}
+  if (!request.headers.has("Authorization")) {
+    throw new BadRequestException("Please provide valid credentials.");
+  }
 
-	const { username, password } = parseBasicAuth(request);
-	const url = new URL(request.url);
-	verifyParameters(url);
+  const { username, password } = parseBasicAuth(request);
+  const url = new URL(request.url);
+  const originalIp =
+    request.headers.get("cf-connecting-ip") ||
+    request.headers.get("x-forwarded-for");
+  verifyParameters(url);
 
-	const response = await informAPI(url, username, password);
-	return response;
+  const response = await informAPI(url, username, password, originalIp);
+  return response;
 }
 
 function verifyParameters(url) {
-	const { searchParams } = url;
+  const { searchParams } = url;
 
-	if (!searchParams) {
-		throw new BadRequestException("You must include proper query parameters");
-	}
+  if (!searchParams) {
+    throw new BadRequestException("You must include proper query parameters");
+  }
 
-	if (!searchParams.get("hostname")) {
-		throw new BadRequestException("You must specify a hostname");
-	}
+  if (!searchParams.get("hostname")) {
+    throw new BadRequestException("You must specify a hostname");
+  }
 
-	if (!(searchParams.get("ip") || searchParams.get("myip"))) {
-		throw new BadRequestException("You must specify an ip address");
-	}
+  if (!(searchParams.get("ip") || searchParams.get("myip"))) {
+    throw new BadRequestException("You must specify an ip address");
+  }
 }
 
-async function informAPI(url, name, token) {
-	const hostnames = url.searchParams.get("hostname").split(",");
-	const ip = url.searchParams.get("ip") || url.searchParams.get("myip");
+async function informAPI(url, name, token, originalIp) {
+  const hostnames = url.searchParams.get("hostname").split(",");
+  const rawIp = url.searchParams.get("ip") || url.searchParams.get("myip");
+  const saneIp = getSaneIp(rawIp, originalIp);
 
-	const cloudflare = new Cloudflare({ token });
+  const cloudflare = new Cloudflare({ token });
 
-	const zone = await cloudflare.findZone(name);
-	for (const hostname of hostnames) {
-		const record = await cloudflare.findRecord(zone, hostname);
-		await cloudflare.updateRecord(record, ip);
-	}
+  const zone = await cloudflare.findZone(name);
+  for (const hostname of hostnames) {
+    const record = await cloudflare.findRecord(zone, hostname);
+    await cloudflare.updateRecord(record, saneIp);
+  }
 
-	return new Response("good", {
-		status: 200,
-		headers: {
-			"Content-Type": "text/plain;charset=UTF-8",
-			"Cache-Control": "no-store",
-		},
-	});
+  return new Response("good", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain;charset=UTF-8",
+      "Cache-Control": "no-store",
+    },
+  });
 }
 
 export default {
-	async fetch(request, env, ctx) {
-		return handleRequest(request).catch((err) => {
-			console.error(err.constructor.name, err);
-			const message = err.reason || err.stack || "Unknown Error";
+  async fetch(request, env, ctx) {
+    return handleRequest(request).catch((err) => {
+      console.error(err.constructor.name, err);
+      const message = err.reason || err.stack || "Unknown Error";
 
-			return new Response(message, {
-				status: err.status || 500,
-				statusText: err.statusText || null,
-				headers: {
-					"Content-Type": "text/plain;charset=UTF-8",
-					"Cache-Control": "no-store",
-					"Content-Length": message.length,
-				},
-			});
-		});
-	},
+      return new Response(message, {
+        status: err.status || 500,
+        statusText: err.statusText || null,
+        headers: {
+          "Content-Type": "text/plain;charset=UTF-8",
+          "Cache-Control": "no-store",
+          "Content-Length": message.length,
+        },
+      });
+    });
+  },
 };
